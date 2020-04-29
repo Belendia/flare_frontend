e7050445262c8fcb6e788b187095509aa0e36646
belendia <belendia@gmail.com>
2020-04-29 16:34:55 +0300

    Add AutocompleteField

---
+++ src/components/AutocompleteField/AutocompleteField.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { TextField } from "@material-ui/core";
+import { useField } from "formik";
+import { fieldToTextField } from "formik-material-ui";
+
+const AutocompleteField = ({ textFieldProps, ...props }) => {
+  const {
+    form: { setTouched, setFieldValue },
+  } = props;
+  const { error, helperText, ...field } = fieldToTextField(props);
+  const { name } = field;
+
+  return (
+    <Autocomplete
+      {...props}
+      {...field}
+      onChange={(_, value) => setFieldValue(name, value)}
+      onBlur={() => setTouched({ [name]: true })}
+      renderInput={(props) => (
+        <TextField
+          {...props}
+          {...textFieldProps}
+          helperText={helperText}
+          error={error}
+        />
+      )}
+    />
+  );
+};
+
+export default AutocompleteField;
